513d498e6593d7600414467d81d05590c93c5ce4
Singaram Senthil <ssingarammech@gmail.com>
2021-03-23 10:18:26 +0530

    fileupdated

---
+++ School.java
@@ -0,0 +1,23 @@
+public class School{
+
+int noOfStudents = 500; 
+int noOfTeachers = 30; 
+
+String cityName = "Chennai"; 
+
+public static void main(String[] args)
+{
+
+
+}
+
+public void admit()
+{
+System.out.println("Admitting Students to School"); 
+}
+public void teach()
+{
+System.out.println("Teaching "); 
+}
+
+}
